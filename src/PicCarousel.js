96ec76dcc4664f762132670b6b9fde66724aae92
KevinWJohnson <kevin.johnson3@cox.net>
2019-11-09 10:31:09 -0800

    Fixed module picturesGroup1.js

--- src/PicCarousel.js
+++ src/PicCarousel.js
@@ -3,50 +3,12 @@ import Carousel from 'react-bootstrap/Carousel'
 import { PicGrp1, PicGroup2 } from './picturesGroup1.js';
 
 class PicCarousel extends React.Component {
-    state = {
-        // pictures: [],
-        pictures: [
-            {
-              title: 'Ocotillo1',
-              author: 'John Smith',
-              imageUrl: require('./images/group1/IMG_0059.JPG'),
-              rotate: '90',
-              width: '460',
-              height: '345',
-            },
-            {
-              title: 'Ocotillo2',
-              author: 'Jane Johnson',
-              imageUrl: require('./images/group1/IMG_0062.JPG'),
-              rotate: '90',
-              width: '460',
-              height: '345',
-            },
-            {
-              title: 'Catus Flower',
-              author: 'Mark Benson',
-              imageUrl: require('./images/group1/IMG_0064.JPG'),
-              rotate: '0',
-              width: '460',
-              height: '345',
-            },
-            {
-              title: 'Mountains',
-              author: 'Maria Lopez',
-              imageUrl: require('./images/group1/IMG_0095.JPG'),
-              rotate: '0',
-              width: '460',
-              height: '345',
-            },
-          ]
-    };
 
     render() {        
-        const { pictures } = this.state
         return (
             <div className='pictureList'>
               <Carousel>
-                {pictures.map((picture, index) => {
+                {PicGrp1.map((picture, index) => {
                   return (
                     <div key={index}>
 
@@ -64,22 +26,6 @@ class PicCarousel extends React.Component {
                       </Carousel.Caption>
                     </div>
                   )
-                  // return (
-                  //   <Carousel.Item key={index}>
-                  //     <img
-                  //       className="d-block w-100"
-                  //       source={require(picture.imageUrl)}
-                  //       alt={picture.title}
-                  //       width={picture.width}
-                  //       height={picture.height}
-                  //       style={{transform: `rotate(${picture.rotate}deg)`}}
-                  //     />
-                  //     <Carousel.Caption>
-                  //       <h3>{picture.title}</h3>
-                  //       <p>{picture.author}</p>
-                  //     </Carousel.Caption>
-                  // </Carousel.Item>
-                  // )
                 })}
               </Carousel>
             </div>
