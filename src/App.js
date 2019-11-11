d36aa5806828f164be41eebb204d2cf3b4107109
KevinWJohnson <kevin.johnson3@cox.net>
2019-11-11 13:31:38 -0800

    Import bootstrap.css in index.js file so that sytles work

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 import PicCarousel from './PicCarousel';
+import TopBar from './TopBar';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div className="carousel">
-          <PicCarousel></PicCarousel>
+          <div className="AppContainer">
+
+          <div className="topbar">
+          <TopBar></TopBar>
+          </div>
+      
+          <div className="carousel">
+            <PicCarousel></PicCarousel>
+          </div>
+
         </div>
-      </div>
       
     );
   }
